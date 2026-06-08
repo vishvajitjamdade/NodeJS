aee1cfc0c56fa3198878925e84fb1840e94356b0
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-06-08 09:52:28 +0530

    nodejs

---
+++ Myserver.js
@@ -0,0 +1,9 @@
+const http = require("http");
+http.createServer(function(req,res){
+    console.log("Welcome To NodeJS");
+    res.write("Welcome To NodeJS");
+    res.write("Good Afternoon");
+    res.end();
+}).listen(5000);
+
+//http:localhost:5000
